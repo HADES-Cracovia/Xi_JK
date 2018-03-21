c31a769666e9cb0ff96e7088cff35695bec58feb
Joanna Kubos <joanna.kubos@ifj.edu.pl>
2018-03-21 13:51:41 +0100

    first commit

---
+++ allChann.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+evt=10000000
+arr=( 090 010 021 022 023 062 100 )
+for i in ${arr[@]}; do
+    echo $i
+    ./vertex_rec out/pluto_chan_${i}_events_25000_seed_???_1_dst_p4500p.root -d ./ -o output_${i}_all1.root -e $evt  &
+done
