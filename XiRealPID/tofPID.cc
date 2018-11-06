26fa793967b66d0b3b4d94d8eab3be0ca3302b25
Joanna Kubos <joanna.kubos@ifj.edu.pl>
2018-11-06 17:14:39 +0100

    better range def

--- XiRealPID/tofPID.cc
+++ XiRealPID/tofPID.cc
@@ -5,9 +5,9 @@ int tofPID(float tof){
     float pi_p_cut = 20.5; //where to put a difference between protons and pions (see drawTof.cc)
     float p2 = 27.; //the upper range of protons in FD
     if(tof != -1){
-	if(tof < pi_p_cut)
+	if(tof > pi1 && tof < pi_p_cut)
 	    vecID = 9;
-	else
+	else if(tof >= pi_p_cut && tof < p2)
 	    vecID = 14;
     
     }//else cout << "tof_v==-1 ---> no PID in FD" << endl;
