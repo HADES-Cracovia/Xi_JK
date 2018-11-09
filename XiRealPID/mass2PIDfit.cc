986e6609ba84c31356f22db8a9e75fc1a02a94fc
Joanna Kubos <joanna.kubos@ifj.edu.pl>
2018-11-09 16:43:37 +0100

    redefinition of pion PID

--- XiRealPID/mass2PIDfit.cc
+++ XiRealPID/mass2PIDfit.cc
@@ -4,13 +4,18 @@ int mass2PIDfit(double fitarr[], float mass2h, float chargeh, int n){
     for(int j = 0; j < n; j++){
 	float mean = fitarr[3*j+1];
 	float sigma = 4*fitarr[3*j+2];
-	if(mass2h*chargeh > (mean-sigma) && mass2h*chargeh < mean+sigma){
+	float a = mass2h*chargeh;
+	if(a > (mean-sigma) && a < mean+sigma){
 	    switch(j){
 	    case 0:
-		partID = 9;
+		if(a < 0)
+		    partID = 9;
+		else partID = 8;
 		break;
 	    case 1:
-		partID = 8;
+		if(a < 0)
+		    partID = 9;
+		else partID = 8;
 		break;
 	    case 2:
 		partID = 11;
